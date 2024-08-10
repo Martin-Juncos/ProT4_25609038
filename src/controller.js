45a1b0ff8863d359fed07c8d90701e46de4f2da6
Martin-Juncos <prof.mcjuncos@gmail.com>
2024-08-10 11:25:09 -0300

    add controller

--- src/controller.js
+++ src/controller.js
@@ -2,8 +2,22 @@ const pool = require("../database/database");
 
 class LibroController {
   async getAll(req, res) {
-    const [result] = await pool.query("SELECT * FROM `libros`");
+    const [result] = await pool.query("SELECT * FROM libros");
     res.json(result);
+  }
+  async add(req, res) {
+    const libro = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO libros (nombre, autor, categoria, `año-publicacion`, ISBN) VALUES (?, ?, ?, ?, ?)",
+      [
+        libro.nombre,
+        libro.autor,
+        libro.categoria,
+        libro["año-publicacion"],
+        libro.ISBN,
+      ]
+    );
+    res.json({ "Id insertado": result.insertId });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const libro = require("./controller");
 const router = express.Router();
 
 router.get("/libros", libro.getAll);
+router.post("/libros", libro.add);
 
 module.exports = router;
