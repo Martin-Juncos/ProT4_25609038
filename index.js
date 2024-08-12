1dac96deeb7089609998a3e1bbb4a2b8e27adc45
Martin-Juncos <prof.mcjuncos@gmail.com>
2024-08-12 08:17:43 -0300

    modificar el puerto

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require("./src/server");
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Corriendo en el puerto: ${port}`));
